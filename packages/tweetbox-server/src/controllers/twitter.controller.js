4f0765a0aaf27717df633ca51388a51f0719077b
Roger Navarro <roger.navarro@sjsu.edu>
2019-10-20 13:03:28 -0700

    feature: Return a single tweet by giving a specified ID parameter.
    FrontEnd
    Dashboard.vue -> add a UI section to send a see tweet.
    
    BackEnd
    twitter.controller.js -> Method that request Tweet by sending its ID.

--- packages/tweetbox-server/src/controllers/twitter.controller.js
+++ packages/tweetbox-server/src/controllers/twitter.controller.js
@@ -69,7 +69,43 @@ export const sendMessageToUser = async function(req, res, next) {
  * 
  */
 export const getTweet = async (req, res, next) => {
-    
+    if(req.session.passport && req.session.passport.user) {
+        try {
+            logger.info('user is logged in.');
+            const tweetID = req.params.id;
+            const twp = await userModel.findOne(
+                {_id: req.session.passport.user._id}
+                ).select('twitterProvider');
+            
+            const T = new Twit({
+                consumer_key: TWITTER.CONSUMER_KEY,
+                consumer_secret: TWITTER.CONSUMER_SECRET,
+                access_token: twp.twitterProvider.token,
+                access_token_secret: twp.twitterProvider.tokenSecret,
+            });
+            T.get(`statuses/show/:id`,{id: tweetID}, (err, result, response) => {
+                if(err) {
+                    logger.error(JSON.stringify(err));
+                    res.status(500).json({
+                        error: 'Something failed. Try again.'
+                    });
+                    next(err);
+                } else {
+                    logger.info(JSON.stringify(result));
+                    res.status(201).json({
+                        message: result.text
+                    })
+                    next();
+                }
+            })
+        } catch(err) {
+            logger.error(err);
+            res.status(500).json({
+                error: err
+            });
+            next(err);
+        }
+    }
 }
 
 export const createTweet = async (req, res, next) => {
